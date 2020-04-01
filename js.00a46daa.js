b85ee2acb2443c8df8a929450f72bec73c7f932d
Claudinei Bido <bido.c@live.com>
2020-04-01 13:44:21 +0200

    Release at Wed Apr  1 13:44:21 CEST 2020

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -137,6 +137,7 @@ function myFunction() {
 
 var video = document.getElementById("video_background");
 video.addEventListener("canplay", function () {
+  console.log("Video can start, but not sure it will play through.");
   setTimeout(function () {
     video.play();
   }, 2000);
@@ -169,7 +170,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33155" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41573" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
